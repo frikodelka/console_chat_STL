ef2fb97bf87b7f8891e4ecd14d5fb0888e3409a9
frikodelka <39860356+frikodelka@users.noreply.github.com>
2023-03-07 00:04:56 +0500

    Add files via upload

---
+++ chat.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "message.h"
+#include <vector>
+#include <map>
+
+template <typename T> class Chat {
+private:
+    std::vector<Message> _messages;
+    std::map <std::string, std::string> _users;
+    std::string currentUser = "";
+
+public:
+    Chat() = default;
+    virtual ~Chat() = default;
+    Chat(Chat& _arr) = delete;
+    Chat& operator=(Chat& _list) = delete;
+
+    void runChat();
+    void userMenu();
+    void addUser();
+    void loginOperation();
+    void logoutOperation();
+    void showUsersByLogin();
+    void setCurrentUser(std::string user);
+    void getCurrentUser() const;
+
+    void createMessage(bool toAll);
+    void showMessages(bool toAll);
+    void showAllMessagesWith();
+    void sentMessages();
+
+};
---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "chat.h"
+#include "chat.cpp"
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    try {
+        Chat<std::string> chat;
+        chat.runChat();
+    }
+    catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
