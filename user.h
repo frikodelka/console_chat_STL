94077da88c09e5c624293236c8d385694b86ee2f
frikodelka <39860356+frikodelka@users.noreply.github.com>
2023-03-06 23:04:13 +0500

    Add files via upload

---
+++ user.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#pragma once
+#include <iostream>
+#include <string>
+
+
+class User
+{
+private:
+    const std::string _login;
+    std::string _password;
+
+public:
+    User() = default;
+    
+
+};
