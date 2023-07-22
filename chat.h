73928fa97c60cbc494b737df64a3a4f4c591e880
frikodelka <39860356+frikodelka@users.noreply.github.com>
2023-07-23 01:29:00 +0500

    Update chat.h

--- chat.h
+++ chat.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "message.h"
 #include <vector>
 #include <map>
@@ -8,12 +8,23 @@ private:
     std::vector<Message> _messages;
     std::map <std::string, std::string> _users;
     std::string currentUser = "";
+    const std::string _login;
+    std::string _password;
 
 public:
     Chat() = default;
     virtual ~Chat() = default;
+    Chat(const std::string& login, const std::string& password)
+        : _login(login), _password(password) {}
     Chat(Chat& _arr) = delete;
     Chat& operator=(Chat& _list) = delete;
+
+    const std::string get_login() { return _login; }
+    bool pwdVerify(const std::string& password) const {
+        if (password == _password)
+            return true;
+        return false;
+    }
 
     void runChat();
     void userMenu();
